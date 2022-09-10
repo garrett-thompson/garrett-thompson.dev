e06b583fadfa6c111957a08d06b9bb6755c1563c
Garrett Thompson <garrmt@gmail.com>
2022-09-10 17:12:54 -0400

    setup done

---
+++ pages/_document.tsx
@@ -0,0 +1,42 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="preload"
+            href="/inter-variable.ttf"
+            as="font"
+            type="font/ttf"
+            crossOrigin={
+              process.env.NODE_ENV === "development" ? "true" : "false" // don't be an Inter font server for the entire internet :)
+            }
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.tsx"],
+  theme: {
+    fontFamily: {
+      inter: ["Inter"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
