595144e14d82d2ca8ccfea0354ec83a3232e9502
Garrett Thompson <garrmt@gmail.com>
2022-10-05 19:42:20 -0700

    update card styles

--- components/card/card.tsx
+++ components/card/card.tsx
@@ -10,7 +10,7 @@ export interface CardProps extends MotionProps {
 export const Card = ({ children, className, ...props }: CardProps) => (
   <motion.div
     className={classNames(
-      "relative px-4 py-6 rounded-xl bg-fuchsia-100 shadow-xl",
+      "relative px-6 py-5 rounded-xl bg-fuchsia-100 shadow-xl",
       className
     )}
     {...props}
