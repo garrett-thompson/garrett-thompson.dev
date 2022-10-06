9749a2aa6c04447573d4db9ccd9404ffa9fbbc67
Garrett Thompson <garrmt@gmail.com>
2022-10-05 19:25:12 -0700

    - fix svg properties - remove unused imports

--- components/card/controls.tsx
+++ components/card/controls.tsx
@@ -62,13 +62,13 @@ const LeftArrowButton = ({ enabled, onClick, className }: ButtonProps) => {
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
-        stroke-width="1.5"
+        strokeWidth="1.5"
         stroke="currentColor"
         className={classNames("w-12 h-12", enabled ? "cursor-pointer" : null)}
       >
         <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           d="M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
         />
       </motion.svg>
@@ -93,13 +93,13 @@ const RightArrowButton = ({ enabled, onClick, className }: ButtonProps) => {
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
-        stroke-width="1.5"
+        strokeWidth="1.5"
         stroke="currentColor"
         className={classNames("w-12 h-12", enabled ? "cursor-pointer" : null)}
       >
         <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
         />
       </motion.svg>
